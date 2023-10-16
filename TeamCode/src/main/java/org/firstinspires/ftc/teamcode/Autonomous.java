aeda8d0e57c91c4a3e874cd88ef3506284ec6baa
PatrookyYT <patrookybiz@gmail.com>
2023-10-15 18:14:39 -0700

    Op modee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -216,19 +216,3 @@ public class Autonomous extends LinearOpMode {
         return String.valueOf(fixedValue);
     }
 }
-
-class robotDirection {
-    double frontLeftPower;
-    double frontRightPower;
-    double backLeftPower;
-    double backRightPower;
-    String direction;
-
-    robotDirection(double frontLeft, double frontRight, double backLeft, double backRight, String directionName) {
-        this.frontLeftPower = frontLeft;
-        this.frontRightPower = frontRight;
-        this.backLeftPower = backLeft;
-        this.backRightPower = backRight;
-        this.direction = directionName;
-    }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode3.java
@@ -2,14 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 //Importing
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 //##################################
 //#                                #
